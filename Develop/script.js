fe92e0162563b5f3673309cd6fdc213dbea93d51
Red Dragon <carsonkeller1995@hotmail.com>
2020-09-19 04:53:43 -0500

    local storage functioning

--- Develop/script.js
+++ Develop/script.js
@@ -20,3 +20,33 @@ var count = 1;
         }
     }
 
+var buttons = document.getElementsByClassName ("btn")
+
+function handleclick(event) {
+  event.preventDefault()
+  console.log(event.target)
+  var buttonid = event.target.dataset.id
+  var input = document.getElementById("inputTask" + buttonid)
+  localStorage.setItem(buttonid, input.value)
+  console.log(localStorage.getItem(buttonid))
+}
+
+
+// render local storage keys to the dom
+// select all buttons on page
+// Attach event listener to the buttons
+// Use local storage to save strings as they appear in the input boxes
+// Create function that pulls button id keys from local storage
+
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", handleclick);
+  console.log(i, localStorage.getItem(i+1))
+  var input = document.getElementById("inputTask" + (i+1))
+  console.log(input)
+  var lstoragevalue = localStorage.getItem(i+1) 
+  if (lstoragevalue !== null) {
+    input.value = lstoragevalue
+  }
+  
+}
+
